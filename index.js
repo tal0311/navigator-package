67f02a477f123e3f3896d8eb2725b50a45a44a08
tal0311 <tal.amit0311@gmail.com>
2022-07-09 17:15:59 +0300

    /

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const myNavigator = {
   getProperties() {
     const msg = `*position will return a promise with location \n
-    *battery will return a promise with an object {charging and battery level} \n
-    *media will take a options for media { audio: boolean, video: boolean } and attributes object \n
-    const videoAttributes = {
-      controls: false,
-      autoplay: true
-    };
-     for displaying the video you will need a <video> with #video \n
+      *battery will return a promise with an object {charging and battery level} \n
+      *media will take a options for media { audio: boolean, video: boolean } and attributes object \n
+         const videoAttributes = {
+         controls: false,
+         autoplay: true
+       };
+      for displaying the video you will need a <video> with #video \n
      *captureImg will able you to capture a media stream to canvas. you will need a <video id="video"> element and a <canvas id="canvas"> element.\n
-     you can activate myNavigator.capture() with a click event
+      you can activate myNavigator.capture() with a click event
     `
     console.log(msg)
   },
